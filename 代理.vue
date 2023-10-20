b70e9ebf4830b1e2a2527008fd9f8444882c51e5
xiaoding <2239058652@qq.com>
2023-10-20 13:24:33 +0800

    代理

---
+++ 代理.vue
@@ -0,0 +1,12 @@
+// const proxyPlugin = require('vite-plugin-proxy')
+
+// 生产环境代理配置
+      // proxyPlugin({
+      //   '/pmspbe': {
+      //     target: 'https://reqres.in',
+      //     changeOrigin: true,
+      //     onProxyRes: (proxyRes) => {
+      //       delete proxyRes.headers['expires']
+      //     },
+      //   },
+      // }),
